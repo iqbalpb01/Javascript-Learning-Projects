66687bd45c0ffc1edefa43a7dbd68dfa2008d6d6
iqbalpb01 <iqbalbasheer646@gmail.com>
2020-08-18 19:46:05 +0530

    completed java script

--- Form-Validator/script.js
+++ Form-Validator/script.js
@@ -4,45 +4,60 @@ const username = document.getElementById('username');
 const email = document.getElementById("Email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const message = document.getElementsByClassName("message");
 
-function showError(input,message){
+function showError(input){
     const formControl = input.parentElement;
     formControl.className = "form-control error";
-}
-function showSuccess(input,message){
+  
+function showSuccess(input){
     const formControl = input.parentElement;
     formControl.className="form-control success";
 }
-
+function checkLength(input,min,max){
+    if(input.value.length<min || checkLength>max){
+        showError(input);
+    }
+    else{
+        showSuccess(input);
+    }
+}
+function passwordMatch(input1,input2){
+    if(input1.value===input2.value){
+        showSuccess(input1);
+        showSuccess(input2);
+    }
+    else{
+        showError(input1);
+        showError(input2);
+    }
+}
+function validateEmail(input) 
+{ 
+ var reg = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+ if (reg.test(input)){
+ showSuccess(input);
+}
+ else{
+    showError(input);
+ } 
+} 
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    if(username.value === ''){
-        showError(username,'Username Required');
-    }
-    else{
-        showSuccess(username);
-    }
+    const formElements = [username,email,password,password2];
+    formElements.forEach(function(item){
+        if(item.value===''){
+            showError(item)
+        }
+        else{
+            showSuccess(item);
+        }
+    });
+    checkLength(username,4,15);
+    checkLength(password,8,20);
+    checkLength(password2,8,20);
+    validateEmail(email);
+});
 
-    if(Email.value === ''){
-        showError(Email,'Email Required');
-    }
-    else{
-        showSuccess(email);
-    }
-
-    if(password.value === ''){
-        showError(password,'Username Required');
-    }
-    else{
-        showSuccess(password);
-    }
-
-    if(password2.value === ''){
-        showError(password2,'Username Required');
-    }
-    else{
-        showSuccess(password2);
-    }
-}); 
 
